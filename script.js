f4955965abb075a2eb1f6bcd21e4c975e5fd70d6
Tyrone Moore <mooret025@gmail.com>
2020-12-03 21:19:23 -0900

    Added a few more functions to script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let url;
 const cardContainer = document.querySelector("#card_container");
+const cardLayout = document.querySelector("#card_layout");
+const resultsContainer = document.querySelector("#results_container");
 document
   .querySelector("#search-form")
   .addEventListener("submit", (event) => getSearch(event));
@@ -18,8 +20,8 @@ function createCards(search) {
   // and one to search gifs.
 
   const card = document.createElement("div");
-  card.style.width = "100px";
-  card.style.height = "100px";
+  card.style.width = "350px";
+  card.style.height = "350px";
   card.style.backgroundColor = "orange";
 
   const title = document.createElement("h3");
@@ -39,4 +41,66 @@ function createCards(search) {
   //   button.textContent = search;
   //   buttonContainer.appendChild(button);
   //   button.addEventListener("click", () => searchGifs(search));
+}
+
+function searchGifs(search) {
+  // set search term
+  let searchTerm = search;
+
+  // query giphy api with search term and capture result
+  // need api key
+  axios
+    .get(
+      `https://api.giphy.com/v1/gifs/search?api_key=[yourkey]&q=${searchTerm}&limit=12`
+    )
+    .then((result) => {
+      const {
+        data: { data },
+      } = result;
+
+      // Clear gifs container
+      cardContainer.innerHTML = "";
+
+      // create img tag forEach gif returned and append to container
+      data.map((gif) => printGifs(gif));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function searchMovies(search) {
+  // set search term
+  let searchTerm = search;
+
+  // query giphy api with search term and capture result
+  // need api key
+  axios
+    .get(`http://img.omdbapi.com/?apikey=[yourkey]&s=${searchTerm}&limit=12`)
+    .then((result) => {
+      const {
+        data: { data },
+      } = result;
+
+      // Clear gifs container
+      resultsContainer.innerHTML = "";
+
+      // create img tag forEach gif returned and append to container
+      data.map((movie) => printGifs(movie));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function printGifs(gif) {
+  const url = gif.images.downsized.url;
+  const photo = document.createElement("IMG");
+  photo.src = url;
+
+  const title = gif.title;
+  const caption = document.createElement("p");
+  caption.innerHTML = title;
+  cardContainer.appendChild(photo);
+  cardContainer.appendChild(caption);
 }
